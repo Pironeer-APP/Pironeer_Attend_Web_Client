a6252ec5314441eae843be9f4fc6079f16129c93
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-06-24 17:00:44 +0900

    [fix]] 회원가입 어드민 페이지로 변경

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -27,6 +27,11 @@ export default function AdminPage() {
         content={"전체 유저 조회"}
         onPress={() => navigate("/users")}
       ></MainButton>
+      <MainButton
+        content={"화원 등록"}
+        onPress={() => navigate("/signup")}
+      ></MainButton>
+
     </InputContainer>
   );
 }
