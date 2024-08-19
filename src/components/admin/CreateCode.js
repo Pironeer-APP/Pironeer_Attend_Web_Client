dede7703d2db5555fcacab81f5ce118a7c19d438
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-08-19 16:07:55 +0900

    [fix] admin session 조회 css 고침

--- src/components/admin/CreateCode.js
+++ src/components/admin/CreateCode.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { MainButton } from "../common/Button";
-import { Container } from "../common/Container";
+import { Container, ContentContainer } from "../common/Container";
 import { PageHeader } from "../common/Header";
 import { COLORS } from "../../utils/theme";
 import { useCreateCode } from "../../viewModel/adminHook";
@@ -20,23 +20,27 @@ const CreateCode = () => {
     },
   ];
   return (
-    <Container>
+    <>
       {isStart ? (
-        <>
+        <Container>
           <PageHeader text={`현재 생성된 코드는 ${code} 입니다.`} buttons={buttons}/>
+          <ContentContainer>
           <MainButton
             content={"강제 종료"}
             onPress={endCode}
             backgroundColor={COLORS.red}
           />
-        </>
+          </ContentContainer>
+        </Container>
       ) : (
-        <>
+        <Container>
           <PageHeader text={`어드민님 반가워요!`} navigateOnClick="/admin" buttons={buttons}/>
+          <ContentContainer>
           <MainButton content={"코드 생성"} onPress={createCode} />
-        </>
+          </ContentContainer>
+        </Container>
       )}
-    </Container>
+    </>
   );
 };
 
