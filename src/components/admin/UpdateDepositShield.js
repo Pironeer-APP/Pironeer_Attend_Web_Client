e78f934416b2872f55c6f9bb9631a8a43e563937
yeonuKim <ywk0524@snu.ac.kr>
2024-08-24 10:37:37 +0900

    [feat] 보증금 방어권 개수 변경 api 연결 완료

--- src/components/admin/UpdateDepositShield.js
+++ src/components/admin/UpdateDepositShield.js
@@ -1,13 +1,16 @@
-
-import React from 'react';
-import styled from 'styled-components';
-import { COLORS } from '../../utils/theme';
-import { useLocation } from 'react-router-dom';
-import { Container, ContentContainer, InputContainer } from '../common/Container';
-import { PageHeader } from '../common/Header';
-import { MainButton } from '../common/Button';
-import { Gap } from '../common/Gap';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../../utils/theme";
+import { useLocation } from "react-router-dom";
+import {
+  Container,
+  ContentContainer,
+  InputContainer,
+} from "../common/Container";
+import { PageHeader } from "../common/Header";
+import { MainButton } from "../common/Button";
+import { Gap } from "../common/Gap";
+import { useNavigate } from "react-router-dom";
 import { useUserDepositDetails, DefendUse } from "../../viewModel/userHook";
 import { useLogin } from "../../viewModel/loginHook";
 import {
@@ -16,11 +19,10 @@ import {
   BalanceAmount,
   BadgeContainer,
   BadgeText,
-} from './DepositForm';
-
+} from "./DepositForm";
 
 const CounterButton = styled.button`
-  background-color: ${(props) => (props.color)};
+  background-color: ${(props) => props.color};
   color: white;
   border: none;
   border-radius: 5px;
@@ -30,53 +32,71 @@ const CounterButton = styled.button`
   cursor: pointer;
 `;
 
-
 const UpdateDepositShield = () => {
   const location = useLocation();
   const { userId } = location.state || {};
-  const { depositData, setDepositData,loading, error } = useUserDepositDetails(userId);    
+  const {
+    depositData,
+    setDepositData,
+    addDepositData,
+    deleteDepositData,
+    loading,
+    error,
+  } = useUserDepositDetails(userId);
   const { onPressLogout } = useLogin();
   const navigate = useNavigate();
   const buttons = [
-      {
-        label: '로그아웃',
-        bgColor: COLORS.orange,
-        color: 'black',
-        onClick: () => onPressLogout(navigate),
-      },
-    ];
-    const increment = () => {
-      alert("보증금 방어권 더하기");
-    }
-  const decrement = () => {
-    alert("보증금 방어권 빼기");
+    {
+      label: "로그아웃",
+      bgColor: COLORS.orange,
+      color: "black",
+      onClick: () => onPressLogout(navigate),
+    },
+  ];
+  const increment = () => {
+    addDepositData();
   };
-  
-    if (loading) return <Container>Loading...</Container>;
-    if (error) return <Container>Error: {error}</Container>;
-    
-  
-    return (
-      <Container>
-        <PageHeader text={`어드민님 반가워요!`} buttons={buttons}  navigateOnClick="/admin" />
-        <ContentContainer >
-          <InputContainer>
-          <BalanceContainer>
-            <BalanceTitle color='white'>{`${depositData.user && depositData.user.username}님의 보증금 현황`}</BalanceTitle>
-            <BalanceAmount color='white'>{depositData && depositData.defendCount}개</BalanceAmount>
-          </BalanceContainer>
-          <Gap />
-        <BadgeContainer bgColor = {`${COLORS.dark_gray}`}>
-        <CounterButton color={`${COLORS.red}`} onClick={decrement}>-</CounterButton>
-          <BadgeText>보증금 방어권 : {depositData && depositData.defendCount}</BadgeText>
-        <CounterButton color={`${COLORS.green}`} onClick={increment}>+</CounterButton>
-        </BadgeContainer>
-        <Gap />
-          <MainButton content={"변경 완료"} onPress={() => alert('변경 완료 clicked')} marginBottom = "2.5"/>
-          </InputContainer>
-        </ContentContainer>
-      </Container>
-    );
+  const decrement = () => {
+    deleteDepositData();
   };
 
-export  default UpdateDepositShield;
+  if (loading) return <Container>Loading...</Container>;
+  if (error) return <Container>Error: {error}</Container>;
+
+  return (
+    <Container>
+      <PageHeader
+        text={`어드민님 반가워요!`}
+        buttons={buttons}
+        navigateOnClick="/admin"
+      />
+      <ContentContainer>
+        <InputContainer>
+          <BalanceContainer>
+            <BalanceTitle color="white">{`${
+              depositData.user && depositData.user.username
+            }님의 보증금 현황`}</BalanceTitle>
+            <BalanceAmount color="white">
+              {depositData && depositData.defendCount}개
+            </BalanceAmount>
+          </BalanceContainer>
+          <Gap />
+          <BadgeContainer bgColor={`${COLORS.dark_gray}`}>
+            <CounterButton color={`${COLORS.red}`} onClick={decrement}>
+              -
+            </CounterButton>
+            <BadgeText>
+              보증금 방어권 : {depositData && depositData.defendCount}
+            </BadgeText>
+            <CounterButton color={`${COLORS.green}`} onClick={increment}>
+              +
+            </CounterButton>
+          </BadgeContainer>
+          <Gap />
+        </InputContainer>
+      </ContentContainer>
+    </Container>
+  );
+};
+
+export default UpdateDepositShield;
--- src/viewModel/userHook.js
+++ src/viewModel/userHook.js
@@ -1,84 +1,82 @@
 import { api } from "../utils/api";
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { checkUserState, checkAttendStart } from '../utils/authentication';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { checkUserState, checkAttendStart } from "../utils/authentication";
 function useAttend() {
-    const [pin, setPin] = useState("");
-    const [warning, setWarning] = useState("");
-  
-    const onChangePin = (value) => {
-      setPin(value);
-    };
-  
-    const onPressAttend = async (navigate, setIsAttend) => {
-      const userId = sessionStorage.getItem("id");
-  
-      // 숫자가 아닌 경우 경고
-      if (isNaN(pin)) {
-        setPin("");
-        setWarning("출석 코드는 0~9의 숫자로 이루어져야 합니다.");
-        return;
-      }
-      // 네자리가 아닌 경우 경고
-      if (pin.length !== 4) {
-        setPin("");
-        setWarning("출석코드는 4자리 숫자로 이루어져야 합니다.");
-        return;
-      }
-      // 정상 입력된 경우 기존의 경고 삭제
-      setWarning("");
-  
-      // 서버에 출석 정보 전달
-      try {
-        const endpoint = `/session/checkAttend/${userId}`;
-        const body = { code: pin };
-  
-        const response = await api.post(endpoint, body);
-        
-        if (response.status === 201) {
-          setIsAttend(true);
-        }
-        setWarning(response.message);
-      }  catch (error) {
-        setWarning(error);
-      }
-      setPin("");
-    };
-  
-    return {
-      pin,
-      warning,
-      onChangePin,
-      onPressAttend,
-    };
-  }
-  const useAttendList = (userId) => {
-    const [attendanceRecords, setAttendanceRecords] = useState([]);
-  
-    useEffect(() => {
-      const fetchAttendance = async () => {
-        try {
-          const response = await api.get(`/user/checkAttendance/${userId}`);
-          console.log("Attendance Response:", response.data);
-          if (response.data && response.data.attendances) {
-            setAttendanceRecords(response.data.attendances);
-          } else {
-            throw new Error("No attendance records found");
-          }
-        } catch (error) {
-          console.error("Error fetching attendance records:", error.message);
-        }
-      };
-  
-      if (userId) {
-        fetchAttendance();
-      }
-    }, [userId]);
-  
-    
-    return { attendanceRecords };
-  
+  const [pin, setPin] = useState("");
+  const [warning, setWarning] = useState("");
+
+  const onChangePin = (value) => {
+    setPin(value);
   };
+
+  const onPressAttend = async (navigate, setIsAttend) => {
+    const userId = sessionStorage.getItem("id");
+
+    // 숫자가 아닌 경우 경고
+    if (isNaN(pin)) {
+      setPin("");
+      setWarning("출석 코드는 0~9의 숫자로 이루어져야 합니다.");
+      return;
+    }
+    // 네자리가 아닌 경우 경고
+    if (pin.length !== 4) {
+      setPin("");
+      setWarning("출석코드는 4자리 숫자로 이루어져야 합니다.");
+      return;
+    }
+    // 정상 입력된 경우 기존의 경고 삭제
+    setWarning("");
+
+    // 서버에 출석 정보 전달
+    try {
+      const endpoint = `/session/checkAttend/${userId}`;
+      const body = { code: pin };
+
+      const response = await api.post(endpoint, body);
+
+      if (response.status === 201) {
+        setIsAttend(true);
+      }
+      setWarning(response.message);
+    } catch (error) {
+      setWarning(error);
+    }
+    setPin("");
+  };
+
+  return {
+    pin,
+    warning,
+    onChangePin,
+    onPressAttend,
+  };
+}
+const useAttendList = (userId) => {
+  const [attendanceRecords, setAttendanceRecords] = useState([]);
+
+  useEffect(() => {
+    const fetchAttendance = async () => {
+      try {
+        const response = await api.get(`/user/checkAttendance/${userId}`);
+        console.log("Attendance Response:", response.data);
+        if (response.data && response.data.attendances) {
+          setAttendanceRecords(response.data.attendances);
+        } else {
+          throw new Error("No attendance records found");
+        }
+      } catch (error) {
+        console.error("Error fetching attendance records:", error.message);
+      }
+    };
+
+    if (userId) {
+      fetchAttendance();
+    }
+  }, [userId]);
+
+  return { attendanceRecords };
+};
 const useUserAttendPage = () => {
   const navigate = useNavigate();
   const [isStart, setIsStart] = useState(false);
@@ -116,39 +114,85 @@ const useUserAttendPage = () => {
 
 const useUserDepositDetails = (userId) => {
   const [depositData, setDepositData] = useState([null]);
-  console.log("useUserDepositDetails userId:",userId)
+  const [loading, setLoading] = useState(true);
+  console.log("useUserDepositDetails userId:", userId);
 
   const [error, setError] = useState(null);
+
+  const fetchDepositData = async () => {
+    try {
+      console.log("Fetching deposit data...");
+      const response = await api.get(`/deposit/${userId}`);
+      setDepositData(response.data);
+      console.log("deposit response:", response.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(true);
+      console.error("Error fetching deposit data:", err.message);
+    }
+  };
+
   useEffect(() => {
-    const fetchDepositData = async () => {
-      try {
-        console.log("Fetching deposit data...");
-        const response = await api.get(`/deposit/${userId}`);
-        setDepositData(response.data)
-        console.log("deposit response:",response.data)
-      } catch (err){
-        setError(err.message);
-        console.error("Error fetching deposit data:", err.message);
-      }
-    };
     if (userId) {
       fetchDepositData();
     }
   }, [userId]);
 
-  return { setDepositData, depositData, error };
+  const addDepositData = async () => {
+    try {
+      const response = await api.post(`/deposit/${userId}/defend/add`);
+      console.log(response.data);
+      alert("보증금이 성공적으로 추가되었습니다.");
+    } catch (err) {
+      alert("보증금 방어권 추가에 실패하였습니다. 다시 시도해주세요.");
+      console.error("Error add deposit data:", err.message);
+    }
+    fetchDepositData();
+  };
+
+  const deleteDepositData = async () => {
+    try {
+      if (depositData.defendCount === 0) {
+        alert("더 이상 보증금을 삭제할 수 없습니다.");
+      } else {
+        const response = await api.post(`/deposit/${userId}/defend/delete`);
+        console.log(response.data);
+        alert("보증금이 성공적으로 삭제되었습니다.");
+        console.log(depositData);
+      }
+    } catch (err) {
+      alert("보증금 방어권 삭제에 실패하였습니다. 다시 시도해주세요.");
+      console.error("Error delete deposit data:", err.message);
+    }
+    fetchDepositData();
+  };
+
+  return {
+    setDepositData,
+    addDepositData,
+    deleteDepositData,
+    depositData,
+    error,
+    loading,
+  };
 };
 
-const DefendUse =  async (userId) => {
-  try{
+const DefendUse = async (userId) => {
+  try {
     const response = await api.post(`/deposit/${userId}/defend/use`);
-    console.log("defend use",response.data);
+    console.log("defend use", response.data);
     return response.data;
-  } catch (err){
+  } catch (err) {
     console.error(err.response?.data?.message);
     throw new Error(err.response?.data?.message);
   }
 };
 
-
-export { useAttend, useAttendList, useUserAttendPage, useUserDepositDetails, DefendUse };
+export {
+  useAttend,
+  useAttendList,
+  useUserAttendPage,
+  useUserDepositDetails,
+  DefendUse,
+};
