624eba9d09d6033058930bba2ed074a19c9201ca
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-08-26 02:19:45 +0900

    [fix] css

--- src/components/admin/CreateCode.js
+++ src/components/admin/CreateCode.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { MainButton } from "../common/Button";
-import { Container, ContentContainer } from "../common/Container";
+import { Container, ContentContainer, InputContainer } from "../common/Container";
 import { PageHeader } from "../common/Header";
 import { COLORS } from "../../utils/theme";
 import { useCreateCode } from "../../viewModel/adminHook";
@@ -26,18 +26,23 @@ const CreateCode = () => {
         <Container>
           <PageHeader text={`현재 생성된 코드는 ${code} 입니다.`} buttons={buttons}/>
           <ContentContainer>
+          <InputContainer>
           <MainButton
             content={"강제 종료"}
             onPress={endCode}
             backgroundColor={COLORS.red}
           />
+          </InputContainer>
+          
           </ContentContainer>
         </Container>
       ) : (
         <Container>
           <PageHeader text={`어드민님 반가워요!`} navigateOnClick="/admin" buttons={buttons}/>
           <ContentContainer>
+          <InputContainer>
           <MainButton content={"코드 생성"} onPress={createCode} />
+          </InputContainer>
           </ContentContainer>
         </Container>
       )}
