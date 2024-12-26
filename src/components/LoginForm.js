5734234f1ae4fd496812a59abb67e7ada0d697b7
chan000518 <wkdvnddl050701@gmail.com>
2024-12-27 00:58:49 +0900

    fix: 로그인 폼 수정
    엔터 치면 로그인 되게 수정
    공백 입력 불가하게 설정

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -20,19 +20,35 @@ export default function LoginForm() {
     onPressLogin,
   } = useLogin();
 
+  // 입력값에서 공백 제거
+  const handleUsernameChange = (text) => {
+    onChangeUsername(text.trim()); // 입력값에서 공백 제거
+  };
+
+  const handlePasswordChange = (text) => {
+    setPassword(text.trim()); // 입력값에서 공백 제거
+  };
+
+  // Enter 키 이벤트 핸들러
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onPressLogin(navigate); // Enter 키를 눌렀을 때 로그인 함수 호출
+    }
+  };
+
   return (
-    <InputContainer>
+    <InputContainer onKeyDown={handleKeyDown}>
       <StyledInput
         placeholder="이름"
         keyboardType="default"
         value={username}
-        onChangeText={onChangeUsername}
+        onChangeText={handleUsernameChange} // 공백 제거 로직 적용
         maxLength={50}
       />
       <StyledInput
         placeholder="비밀번호"
         value={password}
-        onChangeText={setPassword}
+        onChangeText={handlePasswordChange} // 공백 제거 로직 적용
         secureTextEntry={true}
       />
       <MainButton content={"로그인"} onPress={() => onPressLogin(navigate)} />
